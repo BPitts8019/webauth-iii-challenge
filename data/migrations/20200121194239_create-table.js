0dfac7d9559e16a396b37721a175c3b9acee4167
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-21 19:48:41 -0700

    Initialized project with a Users database

---
+++ data/migrations/20200121194239_create-table.js
@@ -0,0 +1,13 @@
+const USERS = "users";
+
+exports.up = async function (knex) {
+   await knex.schema.createTable(USERS, table => {
+      table.increments("id");
+      table.string("username", 128).notNullable().unique();
+      table.string("password", 128).notNullable();
+   });
+};
+
+exports.down = async function (knex) {
+   await knex.schema.dropTableIfExists(USERS);
+};
